c80eb851b8ad95687c30c851ed790f0716abb97a
Artur-Settarov0228 <settarovartur2@gmail.com>
2026-07-17 12:20:05 +0500

    chore: update ALLOWED_HOSTS to permit all hosts in settings

--- core/settings.py
+++ core/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-apic!dxd=*&3=-ny_v*$o##8_e
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False').lower() in ('true', '1', 't')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
